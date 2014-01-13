2c71b200da93f7da6abd99aee745e71a8fca933f
Arturo Filastò <art@fuffa.org>
2014-01-13 15:57:18 +0100

    Update ooniprobe homebrew forumla to latest version.

--- ooni-probe.rb
+++ ooni-probe.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class OoniProbe < Formula
   homepage 'https://github.com/TheTorProject/ooni-probe'
-  url 'https://github.com/TheTorProject/ooni-probe/archive/v0.0.10.tar.gz'
-  sha1 '2028840ef0ccf1eab79012b3c7287b805e573eba'
+  url 'https://github.com/TheTorProject/ooni-probe/archive/v1.0.0-rc6.tar.gz'
+  sha1 'c246d58796340d143d07e41518f52a63d1370fd1'
 
   depends_on :python => 'argparse'
   depends_on :python => 'docutils'
   depends_on :python => 'ipaddr'
-  depends_on :python => 'pygeoip'
+  depends_on :python => 'geoip'
   depends_on :python => 'repoze.sphinx.autointerface'
   depends_on :python => 'txsocksx'
   depends_on :python => 'storm'
@@ -25,17 +25,10 @@ class OoniProbe < Formula
   depends_on :python => ['yaml' =>'PyYAML']
   depends_on :python => ['OpenSSL' => 'pyOpenSSL']
 
+  depends_on 'geoip'
   depends_on 'libdnet'
   depends_on 'scapy'
   depends_on 'pypcap'
-
-  # Fixed a syntax err bug and the requirements.txt seems buggy, too.
-  # So we check with homebrew for all the needed python deps.
-  # The user will need to do a lot of pip install.
-  # Oh and further, there is no `bin/canary`. Is that important?
-  def patches
-    DATA
-  end
 
   def install
     python do
@@ -49,31 +42,3 @@ class OoniProbe < Formula
   end
 
 end
-
-__END__
-diff --git a/nettests/tls-handshake.py b/nettests/tls-handshake.py
-index eba950e..1b1a927 100644
---- a/nettests/tls-handshake.py
-+++ b/nettests/tls-handshake.py
-@@ -22,7 +22,7 @@ ciphers = [
-   "DHE-RSA-CAMELLIA128-SHA",
-   "DHE-DSS-CAMELLIA128-SHA"
- ]
--def checkBridgeConnection(host, port)
-+def checkBridgeConnection(host, port):
-   cipher_arg = ":".join(ciphers)
-   cmd  = ["openssl", "s_client", "-connect", "%s:%s" % (host,port)]
-   cmd += ["-cipher", cipher_arg]
-diff --git a/setup.py b/setup.py
-index 1b8ecc3..cbe219b 100644
---- a/setup.py
-+++ b/setup.py
-@@ -31,7 +31,5 @@ setup(
-     version="0.6",
-     url="http://ooni.nu/",
-     packages=find_packages(),
--    scripts=["bin/canary", "bin/oonib", "bin/ooniprobe"],
--    install_requires=install_requires,
--    dependency_links=dependency_links,
-+    scripts=["bin/oonib", "bin/ooniprobe"]
- )
